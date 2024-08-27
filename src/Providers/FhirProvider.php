82c267bf71bd76fdcd6fa3f2b0a6d48cab6bcb78
Kulcsar Kalman <kalman.kulcsar@lynxsolutions.eu>
2024-08-27 14:23:30 +0300

    Make config file publishable

--- src/Providers/FhirProvider.php
+++ src/Providers/FhirProvider.php
@@ -6,8 +6,15 @@ use Illuminate\Support\ServiceProvider;
 
 class FhirProvider extends ServiceProvider
 {
-    public function boot(): void
+    public function register(): void
     {
         $this->mergeConfigFrom(dirname(__DIR__).'/Config/config.php', 'fhir');
+    }
+
+    public function boot(): void
+    {
+        $this->publishes([
+            dirname(__DIR__).'/Config/config.php' => config_path('fhir.php'),
+        ], 'takepart-laravel-fhir-config');
     }
 }
