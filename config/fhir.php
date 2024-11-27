a64d34845fe62864392d607e0b3ed371e0d4587c
Kulcsar Kalman <kalman.kulcsar@lynxsolutions.eu>
2024-11-27 09:44:50 +0200

    Remove unused config variable

--- config/fhir.php
+++ config/fhir.php
@@ -3,7 +3,4 @@
 return [
     // Used in the FhirValidator class. When set to true, all the validations are ignored when creating a fhir object
     'ignore_validations' => true,
-
-    // Hapi url used to communicate with the hapi server. Other security related variables might be added in the future
-    'hapi_url' => env('HAPI_URL', 'http://localhost'),
 ];
