eee7e421502f2445585252dc2ece6ce9a8d785b1
Kalman Kulcsar <kalman.kulcsar@lynxsolutions.eu>
2026-03-16 15:11:31 +0200

    Add ResearchSubjectResource to return type

--- src/FhirObject.php
+++ src/FhirObject.php
@@ -55,7 +55,7 @@ class FhirObject
         $this->returnValue = $this->build($data['resourceType'], $data);
     }
 
-    public function toFhir(): AbstractResource|PatientResource|BundleResource|OrganizationResource|EncounterResource|PractitionerResource|QuestionnaireResource|QuestionnaireResponseResource|ServiceRequestResource|ObservationResource|ResearchStudyResource
+    public function toFhir(): AbstractResource|PatientResource|BundleResource|OrganizationResource|EncounterResource|PractitionerResource|QuestionnaireResource|QuestionnaireResponseResource|ServiceRequestResource|ObservationResource|ResearchStudyResource|ResearchSubjectResource
     {
         return $this->returnValue;
     }
